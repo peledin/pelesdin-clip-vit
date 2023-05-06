07ce6679afbef9821b3ab1dafd23e15704f0adb4
peledin <dino.pelesevic@gmail.com>
2023-05-06 20:44:53 +0200

    clean up

--- pelesdin-clip/src/main/resources/static/js/main.js
+++ pelesdin-clip/src/main/resources/static/js/main.js
@@ -1,53 +1,90 @@
+// Event-Listener für das Formular-Submit-Event
 document.getElementById("comparison-form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const progressBarContainer = document.getElementById("progress-bar-container");
-  const progressBar = document.getElementById("progress-bar");
-  const resultElement = document.getElementById("result");
-  resultElement.classList.add("d-none");
-  progressBarContainer.classList.remove("d-none");
-  progressBar.style.width = "100%";
+
+  // Zeige den Fortschrittsbalken an und verstecke das Ergebniselement
+  showProgressBar();
+
+  // Werte der Eingabefelder abrufen
   const text1 = document.getElementById("text1").value;
   const text2 = document.getElementById("text2").value;
   const imageUrl = document.getElementById("imageUrl").value;
+
+  // API-Anfrage senden
   const response = await fetch(`/api/compare?text1=${encodeURIComponent(text1)}&text2=${encodeURIComponent(text2)}&imageUrl=${encodeURIComponent(imageUrl)}`);
-  progressBarContainer.classList.add("d-none");
-  progressBar.style.width = "0%";
+
+  // Fortschrittsbalken ausblenden
+  hideProgressBar();
+
+  // Ergebnis anzeigen
   if (response.ok) {
       const probabilities = await response.json();
-      resultElement.innerHTML = `Probabilities: Text 1: ${(probabilities[0] * 100).toFixed(2)}%, Text 2: ${(probabilities[1] * 100).toFixed(2)}%`;
+      displayResult(`Probabilities: Text 1: ${(probabilities[0] * 100).toFixed(2)}%, Text 2: ${(probabilities[1] * 100).toFixed(2)}%`);
   } else {
-      resultElement.innerHTML = "An error occurred.";
+      displayResult("An error occurred.");
   }
-  resultElement.classList.remove("d-none");
 });
 
+function showProgressBar() {
+  const progressBarContainer = document.getElementById("progress-bar-container");
+  const progressBar = document.getElementById("progress-bar");
+  const resultElement = document.getElementById("result");
 
+  resultElement.classList.add("d-none");
+  progressBarContainer.classList.remove("d-none");
+  progressBar.style.width = "100%";
+}
+
+function hideProgressBar() {
+  const progressBarContainer = document.getElementById("progress-bar-container");
+  const progressBar = document.getElementById("progress-bar");
+
+  progressBarContainer.classList.add("d-none");
+  progressBar.style.width = "0%";
+}
+
+function displayResult(message) {
+  const resultElement = document.getElementById("result");
+
+  resultElement.innerHTML = message;
+  resultElement.classList.remove("d-none");
+}
+
+// Event-Listener für den Shuffle-Button
+document.getElementById("shuffleButton").addEventListener("click", () => {
+  const randomImage = getRandomImage();
+  updateImagePreview(randomImage);
+});
+
+// Funktion zum Abrufen eines zufälligen Bildes aus dem cocoImages-Array
 function getRandomImage() {
   const index = Math.floor(Math.random() * cocoImages.length);
   return cocoImages[index];
 }
 
-document.getElementById("shuffleButton").addEventListener("click", () => {
-  const randomImage = getRandomImage();
-  document.getElementById("imagePreview").src = randomImage;
-  document.getElementById("imageUrl").value = randomImage;
-});
+// Funktion zum Aktualisieren der Bildvorschau und des imageURL-Eingabefeldes
+function updateImagePreview(url) {
+  document.getElementById("imagePreview").src = url;
+  document.getElementById("imageUrl").value = url;
+}
 
-const cocoImages = [
-  "http://images.cocodataset.org/val2017/000000039769.jpg",
-  "http://images.cocodataset.org/val2017/000000252219.jpg",
-  "http://images.cocodataset.org/val2017/000000000285.jpg",       
-  "http://images.cocodataset.org/val2017/000000000724.jpg",
-  "http://images.cocodataset.org/val2017/000000002149.jpg",
-  "http://images.cocodataset.org/val2017/000000002299.jpg",
-  "http://images.cocodataset.org/val2017/000000001296.jpg",
-  "http://images.cocodataset.org/val2017/000000002153.jpg",
-  "http://images.cocodataset.org/val2017/000000001000.jpg",
-  "http://images.cocodataset.org/val2017/000000008532.jpg",
-  // ...
-];
-
+// Event-Listener für das Ändern des imageURL-Eingabefeldes
 document.getElementById("imageUrl").addEventListener("input", () => {
   const imageUrl = document.getElementById("imageUrl").value;
   document.getElementById("imagePreview").src = imageUrl;
 });
+
+    // Liste der verfügbaren Bilder aus dem COCO-Datensatz
+    const cocoImages = [
+      "http://images.cocodataset.org/val2017/000000039769.jpg",
+      "http://images.cocodataset.org/val2017/000000252219.jpg",
+      "http://images.cocodataset.org/val2017/000000000285.jpg",
+      "http://images.cocodataset.org/val2017/000000000724.jpg",
+      "http://images.cocodataset.org/val2017/000000002149.jpg",
+      "http://images.cocodataset.org/val2017/000000002299.jpg",
+      // Weitere URLs hier hinzufügen
+  ];
+
+  // Initialisiere die Bildvorschau und das imageURL-Eingabefeld mit einem zufälligen Bild
+  const initialImage = getRandomImage();
+  updateImagePreview(initialImage);
